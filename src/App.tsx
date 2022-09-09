b35537c8bfa4ae35a183b0d67a5b711ead81fcc2
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2022-09-09 15:52:39 +0300

    app base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import { AppBar } from '@mui/material';
 
-function App() {
+// create theme mui
+const theme = {
+  palette: {
+    primary: {
+      main: '#1976d2'
+    },
+    secondary: {
+      main: '#dc004e'
+    }
+  }
+};
+// add an appbar to the app component
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
